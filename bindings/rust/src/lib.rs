c5d8af00642135d7ee165a71e2b47fc88c5ff37f
Wolfgang Welz <welzwo@gmail.com>
2025-03-21 15:56:45 +0100

    remove articats

--- bindings/rust/src/lib.rs
+++ bindings/rust/src/lib.rs
@@ -7,7 +7,6 @@ extern crate alloc;
 // Without it, the compiler won't link to `blst` when compiling this crate.
 // See: https://kornel.ski/rust-sys-crate#linking
 extern crate blst;
-extern crate core;
 
 mod bindings;
 
